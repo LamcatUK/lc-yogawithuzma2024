81b99880cfacd8a628c289f55d2946b3e0ff2231
ChillibyteUK <136230172+LamcatUK@users.noreply.github.com>
2024-05-12 12:50:54 +0100

    Update lc_sessions.php

--- page-templates/blocks/lc_sessions.php
+++ page-templates/blocks/lc_sessions.php
@@ -7,11 +7,12 @@
             while (have_rows('sessions')) {
                 the_row();
                 $l = get_sub_field('link');
+                $img = wp_get_attachment_image(get_sub_field('image'), 'full', false, array( 'class' => 'sessions__image w-100' )) ?? '<img src="' . get_stylesheet_directory_uri() . '/img/missing-image.png" class="sessions__image w-100>';
                 ?>
             <div class="col-md-6 col-xl-3">
                 <a class="sessions__card shadow-1"
                     href="<?=$l['url']?>">
-                    <?=wp_get_attachment_image(get_sub_field('image'), 'full', false, array( 'class' => 'sessions__image w-100' ));?>
+                    <?=$img?>
                     <div class="sessions__title shadow-1">
                         <?=$l['title']?>
                     </div>
