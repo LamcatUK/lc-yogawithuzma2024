db0898e5d6d1adf62d19c49ea759cd66f96ba4bb
ChillibyteUK <136230172+LamcatUK@users.noreply.github.com>
2024-05-27 13:28:40 +0100

    testimonials

--- page-templates/blocks/lc_all_testimonials.php
+++ page-templates/blocks/lc_all_testimonials.php
@@ -12,20 +12,15 @@ while ($q->have_posts()) {
     $q->the_post();
     ?>
         <a id="<?=acf_slugify(get_the_title())?>" class="anchor"></a>
-        <div class="row g-5">
-            <div class="col-md-3">
-                <?=get_the_post_thumbnail(get_the_ID(), 'full', array( 'class' => 'd-block w-100 rounded-lg all_testimonials__image' ))?>
+        <div class="testimonials__words">
+            <div class="testimonials__content">
+                <?=get_the_content()?>
             </div>
-            <div class="col-md-9 testimonials__words">
-                <div class="testimonials__content">
-                    <?=get_the_content()?>
-                </div>
-                <div class="testimonials__title text-center text-md-end">
-                    <?=get_the_title()?>
-                </div>
-                <div class="testimonials__loca text-center text-md-end">
-                    <?=get_field('location', get_the_ID())?>
-                </div>
+            <div class="testimonials__title text-center">
+                <?=get_the_title()?>
+            </div>
+            <div class="testimonials__loca text-center">
+                <?=get_field('location', get_the_ID())?>
             </div>
         </div>
         <hr class="my-5">
--- page-templates/blocks/lc_testimonials.php
+++ page-templates/blocks/lc_testimonials.php
@@ -2,8 +2,8 @@
     <div class="testimonials__overlay"></div>
     <div class="container-xl">
         <h2 class="text-center mb-4 text-purple-400">Testimonials</h2>
-        <div class="testimonials__carousel carousel slide carousel-fade w-lg-75 mx-auto" data-bs-ride="carousel">
-            <div class="carousel-inner">
+        <div class="testimonials__carousel carousel slide carousel-fade w-lg-75 mx-auto"><!--data-bs-ride="carousel" -->
+            <div class="carousel-inner pb-4">
                 <?php
 
             $q = new WP_Query(array(
@@ -17,20 +17,15 @@
                 <div class="carousel-item <?=$a?>">
                     <a
                         href="/testimonials/#<?=acf_slugify(get_the_title())?>">
-                        <div class="row g-5">
-                            <div class="col-md-3">
-                                <?=get_the_post_thumbnail(get_the_ID(), 'full', array( 'class' => 'd-block w-100 rounded-lg testimonials__image' ))?>
+                        <div class="testimonials__words">
+                            <div class="testimonials__content">
+                                <?=get_the_content()?>
                             </div>
-                            <div class="col-md-9 testimonials__words">
-                                <div class="testimonials__content">
-                                    <?=get_the_content()?>
-                                </div>
-                                <div class="testimonials__title text-center text-md-end">
-                                    <?=get_the_title()?>
-                                </div>
-                                <div class="testimonials__loca text-center text-md-end">
-                                    <?=get_field('location', get_the_ID())?>
-                                </div>
+                            <div class="testimonials__title text-center">
+                                <?=get_the_title()?>
+                            </div>
+                            <div class="testimonials__loca text-center">
+                                <?=get_field('location', get_the_ID())?>
                             </div>
                         </div>
                     </a>
