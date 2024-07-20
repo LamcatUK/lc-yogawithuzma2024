563e086b069ea1516f1a3330fcdc946931253da1
LamcatUK <136230172+LamcatUK@users.noreply.github.com>
2024-07-20 12:35:57 +0100

    separator

--- inc/lc-blocks.php
+++ inc/lc-blocks.php
@@ -83,6 +83,15 @@ function acf_blocks()
             'mode'	=> 'edit',
             'supports' => array('mode' => false),
         ));
+        acf_register_block_type(array(
+            'name'				=> 'lc_separator',
+            'title'				=> __('LC Separator'),
+            'category'			=> 'layout',
+            'icon'				=> 'cover-image',
+            'render_template'	=> 'page-templates/blocks/lc_separator.php',
+            'mode'	=> 'edit',
+            'supports' => array('mode' => false),
+        ));
     }
 }
 add_action('acf/init', 'acf_blocks');
---
+++ page-templates/blocks/lc_separator.php
@@ -0,0 +1,7 @@
+<aside class="separator py-5">
+    <div class="container-xl text-center">
+        <div class="separator__left"></div>
+        <img src="<?=get_stylesheet_directory_uri()?>/img/ywu-icon.svg" alt="" width=200 height=105>
+        <div class="separator__right"></div>
+    </div>
+</aside>
