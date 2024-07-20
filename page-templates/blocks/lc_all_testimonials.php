a1aaf3e31ea690a8878fd0d16d2b2592c0a56efe
LamcatUK <136230172+LamcatUK@users.noreply.github.com>
2024-07-20 18:44:44 +0100

    Update lc_all_testimonials.php

--- page-templates/blocks/lc_all_testimonials.php
+++ page-templates/blocks/lc_all_testimonials.php
@@ -23,7 +23,13 @@ while ($q->have_posts()) {
                 <?=get_field('location', get_the_ID())?>
             </div>
         </div>
-        <hr class="my-5">
+        <aside class="separator py-5">
+            <div class="container-xl text-center">
+                <div class="separator__left"></div>
+                <img src="<?=get_stylesheet_directory_uri()?>/img/ywu-icon.svg" alt="" width=200 height=105>
+                <div class="separator__right"></div>
+            </div>
+        </aside>
         <?php
 }
 ?>
