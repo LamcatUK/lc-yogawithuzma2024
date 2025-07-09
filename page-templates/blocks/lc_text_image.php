dc3812572b435fd44094645098622ebcbaa786df
LamcatUK <git@lamcat.co.uk>
2025-07-09 20:18:11 +0100

    second link on text image

--- page-templates/blocks/lc_text_image.php
+++ page-templates/blocks/lc_text_image.php
@@ -1,102 +1,130 @@
 <?php
-$txtcol = get_field('order') == 'text/image' ? 'order-2 order-lg-1' : 'order-2 order-lg-2';
-$imgcol = get_field('order') == 'text/image' ? 'order-1 order-lg-2' : 'order-1 order-lg-1';
+/**
+ * Text Image Block Template
+ *
+ * @package lc-yogawithuzma2024
+ */
 
-$txtcolwidth = get_field('split') == '5050' ? 'col-lg-6' : 'col-lg-9';
-$imgcolwidth = get_field('split') == '5050' ? 'col-lg-6' : 'col-lg-3';
+defined( 'ABSPATH' ) || exit;
 
-$overlay_alignment = get_field('order') == 'text/image' ? 'text_image__overlay--left' : 'text_image__overlay--right';
+$txtcol = 'text/image' === get_field( 'order' ) ? 'order-2 order-lg-1' : 'order-2 order-lg-2';
+$imgcol = 'text/image' === get_field( 'order' ) ? 'order-1 order-lg-2' : 'order-1 order-lg-1';
 
-$colour = get_field('background') ?: 'white';
-$title = 'text-purple-400';
-$shadow = 'shadow-1';
+$txtcolwidth = '5050' === get_field( 'split' ) ? 'col-lg-6' : 'col-lg-9';
+$imgcolwidth = '5050' === get_field( 'split' ) ? 'col-lg-6' : 'col-lg-3';
+
+$overlay_alignment = 'text/image' === get_field( 'order' ) ? 'text_image__overlay--left' : 'text_image__overlay--right';
+
+$colour    = get_field( 'background' ) ? get_field( 'background' ) : 'white';
+$title_col = 'text-purple-400';
+$shadow    = 'shadow-1';
 
 $class = $block['className'] ?? 'py-5';
 
-switch ($colour) {
+switch ( $colour ) {
     case 'white':
-        $content = '';
-        $title = 'text-purple-400';
-        $shadow = '';
+        $content   = '';
+        $title_col = 'text-purple-400';
+        $shadow    = '';
         break;
     case 'teal-400':
-        $content = 'text-white';
-        $title = 'text-white';
+        $content   = 'text-white';
+        $title_col = 'text-white';
         break;
     case 'purple-400':
-        $content = 'text-purple-400';
-        $title = 'text-white';
+        $content   = 'text-purple-400';
+        $title_col = 'text-white';
         break;
     default:
-        $content = '';
-        $title = 'text-purple-400';
+        $content   = '';
+        $title_col = 'text-purple-400';
         break;
 }
 
-$img = wp_get_attachment_image(get_field('image'), 'full', false, array('class' => 'rounded-lg w-100 shadow-1')) ?: '<img src="' . get_stylesheet_directory_uri() . '/img/placeholder-400x300.jpg" class="rounded-lg">';
+$img = wp_get_attachment_image(
+    get_field( 'image' ),
+    'full',
+    false,
+    array(
+        'class' => 'rounded-lg w-100 shadow-1',
+    )
+);
 
-if ($colour == 'white') {
-    ?>
-<div class="buffer-top-sm bg-white"></div>
-<?php
+if ( ! $img ) {
+    $img = '<img src="' . esc_url( get_stylesheet_directory_uri() ) . '/img/placeholder-400x300.jpg" class="rounded-lg">';
 }
 
-$anchor = isset($block['anchor']) ? $block['anchor'] : '';
-if ($anchor) {
+if ( 'white' === $colour ) {
     ?>
-<a id="<?=$anchor?>" class="anchor"></a>
-<?php
+<div class="buffer-top-sm bg-white"></div>
+    <?php
+}
+
+$anchor = isset( $block['anchor'] ) ? $block['anchor'] : '';
+if ( $anchor ) {
+    ?>
+<a id="<?= esc_attr( $anchor ); ?>" class="anchor"></a>
+    <?php
 }
 ?>
 
 <section
-    class="text_image <?=$shadow?> <?=$class?> bg-<?=$colour?>">
+    class="text_image <?= esc_attr( $shadow ); ?> <?= esc_attr( $class ); ?> bg-<?= esc_attr( $colour ); ?>">
     <?php
-    $overlay = get_field('overlay');
-if (is_array($overlay) && !empty($overlay) && $overlay[0] == 'Yes') {
+    $overlay = get_field( 'overlay' );
+    if ( is_array( $overlay ) && ! empty( $overlay ) && 'Yes' === $overlay[0] ) {
+        ?>
+    <div class="text_image__overlay <?= esc_attr( $overlay_alignment ); ?>"></div>
+        <?php
+    }
     ?>
-    <div class="text_image__overlay <?=$overlay_alignment?>"></div>
-    <?php
-}
-?>
     <div class="container-xl">
         <?php
-        if (get_field('title') ?? null) {
+        if ( get_field( 'title' ) ?? null ) {
             ?>
-        <div class="h2 text-center d-lg-none mb-4 <?=$title?>">
-            <?=get_field('title')?>
+        <div class="h2 text-center d-lg-none mb-4 <?= esc_attr( $title_col ); ?>">
+            <?= esc_html( get_field( 'title' ) ); ?>
         </div>
-        <?php
+            <?php
         }
-?>
+        ?>
         <div class="row">
             <div
-                class="<?=$txtcolwidth?> d-flex flex-column justify-content-center align-items-start mb-4 <?=$txtcol?>">
+                class="<?= esc_attr( $txtcolwidth ); ?> d-flex flex-column justify-content-center align-items-start mb-4 <?= esc_attr( $txtcol ); ?>">
                 <?php
-    if (get_field('title') ?? null) {
-        ?>
-                <h2 class="d-none d-lg-block <?=$title?>">
-                    <?=get_field('title')?>
+                if ( get_field( 'title' ) ?? null ) {
+                    ?>
+                <h2 class="d-none d-lg-block <?= esc_attr( $title_col ); ?>">
+                    <?= esc_html( get_field( 'title' ) ); ?>
                 </h2>
-                <?php
-    }
-?>
-                <div><?=the_field('content')?>
+                    <?php
+                }
+                ?>
+                <div><?= wp_kses_post( get_field( 'content' ) ); ?></div>
+                <div class="d-flex w-100 flex-wrap align-items-center justify-content-center gap-4">
+                    <?php
+                    if ( get_field( 'link' ) ?? null ) {
+                        $l = get_field( 'link' );
+                        ?>
+                    <a href="<?= esc_url( $l['url'] ); ?>"
+                        target="<?= esc_attr( $l['target'] ); ?>"
+                        class="mt-4 btn btn-primary"><?= esc_attr( $l['title'] ); ?></a>
+                        <?php
+                    }
+                    if ( get_field( 'link_2' ) ?? null ) {
+                        $l = get_field( 'link_2' );
+                        ?>
+                    <a href="<?= esc_url( $l['url'] ); ?>"
+                        target="<?= esc_attr( $l['target'] ); ?>"
+                        class="mt-4 btn btn-primary"><?= esc_attr( $l['title'] ); ?></a>
+                        <?php
+                    }
+                    ?>
                 </div>
-                <?php
-if (get_field('link') ?? null) {
-    $l = get_field('link');
-    ?>
-                <a href="<?=$l['url']?>"
-                    target="<?=$l['target']?>"
-                    class="mt-4 btn btn-primary mx-auto"><?=$l['title']?></a>
-                <?php
-}
-?>
             </div>
             <div
-                class="<?=$imgcolwidth?> <?=$imgcol?> mb-4 d-flex align-items-center justify-content-center">
-                <?=$img?>
+                class="<?= esc_attr( $imgcolwidth ); ?> <?= esc_attr( $imgcol ); ?> mb-4 d-flex align-items-center justify-content-center">
+                <?= wp_kses_post( $img ); ?>
             </div>
         </div>
     </div>
